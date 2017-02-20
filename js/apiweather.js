5d736f3fb543363c6c4a823457905da311b92ec2
YuraKhan <m_200_6@mail.ru>
2017-02-20 15:58:28 +0300

    UpdateApiWorking

--- js/apiweather.js
+++ js/apiweather.js
@@ -1,42 +1,34 @@
 var apiAddress = 'http://api.openweathermap.org/data/2.5/forecast';
 var appId = '199e51a7251d81ae172475ea5b313f94';
 
-//НЕ ЮЗАТЬ
-function getWeather(params,onError,onComplete){
+// НЕ ЮЗАТЬ
+function getWeather(params,onSuccess,onError, onComplete) {
 	$(document).ready(function() {
 		$.ajax({
 			url : apiAddress,
-			type :   'GET',
+			type : 'GET',
 			dataType : 'json',
-			data: params,
-			success : function(json) {
-				/*$.each(json,function(key,val){
-					console.log(key+" "+val);
-				});*/
-				return json;
-			},
+			data : params,
+			success : onSuccess,
 			error : onError,
 			complete : onComplete
 		});
 	});
 }
 
-//Выводит погоду по названию
-function getWeatherByName(nameCity,onError,onComplete) {
+// Выводит погоду по названию
+function getWeatherByName(nameCity,onSuccess,onError, onComplete) {
 	return getWeather({
-		'q': nameCity,
-		'APPID':appId
-	},onError,onComplete);
+		'q' : nameCity,
+		'APPID' : appId
+	},onSuccess,onError,onComplete);
 }
 
-//Выводит погоду по координатам. Можно припилить GPS.
-function getWeatherByCoordinates(lat,lon,onError,onComplete){
+// Выводит погоду по координатам. Можно припилить GPS.
+function getWeatherByCoordinates(lat, lon,onSuccess,onError,onComplete) {
 	return getWeather({
-		'lat':lat,
-		'lon':lon,
-		'APPID':appId
-	},onError,onComplete);
+		'lat' : lat,
+		'lon' : lon,
+		'APPID' : appId
+	},onSuccess,onError,onComplete);
 }
-
-
-
