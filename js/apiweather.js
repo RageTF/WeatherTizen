fdc0c922d65f883da4eeb6e0608808ebd61755a8
ruslan <freelanzer28.mail.ru>
2017-02-23 22:19:16 +0300

    first

---
+++ js/apiweather.js
@@ -0,0 +1,34 @@
+var apiAddress = 'http://api.openweathermap.org/data/2.5/weather';
+var appId = '199e51a7251d81ae172475ea5b313f94';
+
+// НЕ ЮЗАТЬ
+function getWeather(params,onSuccess,onError, onComplete) {
+	$(document).ready(function() {
+		$.ajax({
+			url : apiAddress,
+			type : 'GET',
+			dataType : 'json',
+			data : params,
+			success : onSuccess,
+			error : onError,
+			complete : onComplete
+		});
+	});
+}
+
+// Выводит погоду по названию
+function getWeatherByName(nameCity, onSuccess, onError, onComplete) {
+	return getWeather({
+		'q' : nameCity,
+		'APPID' : appId
+	},onSuccess,onError,onComplete);
+}
+
+// Выводит погоду по координатам. Можно припилить GPS.
+function getWeatherByCoordinates(lat, lon,onSuccess,onError,onComplete) {
+	return getWeather({
+		'lat' : lat,
+		'lon' : lon,
+		'APPID' : appId
+	},onSuccess,onError,onComplete);
+}
---
+++ js/main.js
@@ -0,0 +1,67 @@
+var backEventListener = null;
+
+var unregister = function() {
+    if ( backEventListener !== null ) {
+        document.removeEventListener( 'tizenhwkey', backEventListener );
+        backEventListener = null;
+        window.tizen.application.getCurrentApplication().exit();
+    }
+}
+
+//Initialize function
+var init = function () {
+    // register once
+    if ( backEventListener !== null ) {
+        return;
+    }
+    
+    // TODO:: Do your initialization job
+    console.log("init() called");
+    
+    var backEvent = function(e) {
+        if ( e.keyName == "back" ) {
+            try {
+                if ( $.mobile.urlHistory.activeIndex <= 0 ) {
+                    // if first page, terminate app
+                    unregister();
+                } else {
+                    // move previous page
+                    $.mobile.urlHistory.activeIndex -= 1;
+                    $.mobile.urlHistory.clearForward();
+                    window.history.back();
+                }
+            } catch( ex ) {
+                unregister();
+            }
+        }
+    }
+    
+    $("#btn_search").bind("click", function(event, ui){
+    	var cityName=$("#city_name").val();
+    	getWeatherByName(cityName,onSuccess,onError,onComplete);
+    });
+    
+    // add eventListener for tizenhwkey (Back Button)
+    document.addEventListener( 'tizenhwkey', backEvent );
+    backEventListener = backEvent;
+};
+
+var onSuccess = function(data) {
+	$("#city_name_result").text(data.name);
+	var temp=(data.main.temp - 273.15).toFixed(1);
+	var image="http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+	$("#weather_image").attr("src", image);
+	$("#weather_image").attr("height", 100);
+	$("#weather_image").attr("width", 100);
+	$("#current_temp").text(temp+" C");
+}
+var onError = function() {
+	$("#city_name_result").text("Error");
+}
+var onComplete = function() {
+}
+
+$(document).bind( 'pageinit', init );
+$(document).unload( unregister );
+
+
