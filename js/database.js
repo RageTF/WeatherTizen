0493f6cdbd13e588dd9041c57ae637191be661a3
YuraKhan <m_200_6@mail.ru>
2017-02-23 17:09:11 +0300

    DatabaseJS

---
+++ js/database.js
@@ -0,0 +1,100 @@
+var database;
+var version = 1.0;
+var databaseName = "weather_data";
+var databaseDisplayName = "Weather Database";
+var databaseSize = 2 * 1024 * 1024;
+
+var weatherDataTableName = "weather";
+
+/*
+ * Вызвать перед обращением к базе данных!
+ */
+function openDB() {
+	database = openDatabase(databaseName, version, databaseDisplayName,
+			databaseSize);
+	database
+			.transaction(function(transaction) {
+				transaction
+						.executeSql("CREATE TABLE IF NOT EXISTS "
+								+ weatherDataTableName
+								+ "(city_id TEXT PRIMARY KEY NOT NULL,city_name TEXT NOT NULL, city_weather TEXT);");
+			});
+}
+
+/*
+ * onSuccessInsert - вызывается при успешном добавлении. onSuccesInsert - при
+ * неудачном добавлении.
+ */
+
+/*
+ * Занести запись в базу данных. -- idCity - id города из JSON. -- nameCity -
+ * название города. -- weatherCity - JSON в текстовом формате.
+ */
+function putWeatherCity(idCity, nameCity, weatherCity, onSuccessInsert,
+		onErrorInsert) {
+	database
+			.transaction(function(transaction) {
+				transaction
+						.executeSql(
+								("INSERT INTO " + weatherDataTableName + " (city_id,city_name,city_weather) VALUES(?,?,?)"),
+								[ idCity, nameCity, weatherCity ],
+								onSuccessInsert, onErrorInsert);
+			});
+}
+
+/*
+ * OnSuccessCallback(result) вызывается при успешном обращении к базе данных. --
+ * result - данные полученные в результате запроса.
+ * 
+ * OnErrorCallback вызывается при неудачном запросе. Или если по запросу ничего не найдено!
+ */
+
+/*
+ * Получить погоду города по id.
+ */
+function getWeatherFromDatabaseByCityId(idCity, onSuccessCallback,
+		onErrorCallback) {
+	database.transaction(function(transaction) {
+		transaction.executeSql(
+				("SELECT * FROM " + weatherDataTableName + " WHERE city_id=?"),
+				[ idCity ], function(transaction, results) {
+					var count = results.rows.length;
+					if (count > 0) {
+						onSuccessCallback(results);
+					} else {
+						onErrorCallback();
+					}
+				}, onErrorCallback);
+	});
+}
+
+/*
+ * Получить все записи о погоде из базы данных.
+ */
+function getWeatherFromDatabase(onSuccessCallback, onErrorCallback) {
+	database.transaction(function(transaction) {
+		transaction.executeSql(("SELECT * FROM " + weatherDataTableName), [],
+				function(transaction, results) {
+					var count = results.rows.length;
+					if (count > 0) {
+						onSuccessCallback(results);
+					} else {
+						onErrorCallback();
+					}
+
+				}, onErrorCallback);
+	});
+}
+
+/*
+ * Все понятно! Обновляем записи в базе данных!
+ * бновляет JSON города оп id;
+ */
+function updateWeatherByCityId(idCity, weatherJSON, onSuccessUpdate,onErrorUpdate) {
+	database.transaction(function(transaction) {
+				transaction.executeSql(
+								("UPDATE " + weatherDataTableName + " SET city_weather=? WHERE city_id=?"),
+								[ weatherJSON, idCity ], onSuccessUpdate,
+								onErrorUpdate);
+			});
+}
